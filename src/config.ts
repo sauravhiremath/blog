370b2456129d5a1511dea5140662071b38b987ce
Saurav M Hiremath <sauravhiremath@gmail.com>
2024-09-22 03:09:44 +0530

    feat: config updates

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const SITE_FAVICON = "/favicon.png";
 export const SITE_LOGO = "/avatar.png";
 export const SITE_TITLE = "Saurav Blogs";
 export const SITE_DESCRIPTION =
-  "Mia is designed to be minimalist yet powerful, integrating the capabilities of Astro, MDX, and Rough Notation. Whether you're a developer, writer, or just someone who loves clean and efficient design, this theme is perfect for you.";
+  "Blogs by Saurav M. H · Web Development · Security · DevOps";
 
 export const MENUS = [
   {
